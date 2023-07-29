abad836fed373872bcf66726ce3f812ea1f1a540
magiczny <magicznyjasio@gmail.com>
2023-07-29 11:37:53 +0200

    First commit

---
+++ src/test/java/game/hearthstone/HearthstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package game.hearthstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearthstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

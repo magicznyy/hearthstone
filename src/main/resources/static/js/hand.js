aec04870e34eaa3602b5508a842d54fba3f21dc2
magiczny <magicznyjasio@gmail.com>
2023-11-07 17:35:52 +0100

    cards position in hand edited

--- src/main/resources/static/js/hand.js
+++ src/main/resources/static/js/hand.js
@@ -3,7 +3,7 @@ window.addEventListener('load',printCards)
 function printCards() {
 var text = ""
 for(var i=0;i<=7;i++)
-  text += "<div id = card" + i  + " class=\"card\"><img src=/images/card1.png alt=\"cameleon\"> jeden </div>"
+  text += "<div id = card" + i  + " class=\"card\" draggable=\"true\"><img src=/images/card1.png alt=\"cameleon\"></div>"
 
   document.getElementById("hand").innerHTML = text;
 }
